9b316698397bae945aed39274b3fd85143e75eef
Aash <aash.shah@somaiya.edu>
2022-07-21 13:00:30 +0530

    first commit

---
+++ myportal/users/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash 
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib import messages 
+from .forms import SignUpForm
+# Create your views here.
+def home(request): 
+	return render(request, 'users/home.html', {})
+
+def login_user (request):
+	if request.method == 'POST': #if someone fills out form , Post it 
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:# if user exist
+			login(request, user)
+			messages.success(request,('Youre logged in'))
+			return redirect('home') #routes to 'home' on successful login  
+		else:
+			messages.success(request,('Error logging in'))
+			return redirect('login') #re routes to login page upon unsucessful login
+	else:
+		return render(request, 'users/login.html', {})
+
+def logout_user(request):
+	logout(request)
+	messages.success(request,('Youre now logged out'))
+	return redirect('home')
+
+def register_user(request):
+	if request.method =='POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request,user)
+			messages.success(request, ('Youre now registered'))
+			return redirect('home')
+	else: 
+		form = SignUpForm() 
+
+	context = {'form': form}
+	return render(request, 'users/register.html', context)
+
